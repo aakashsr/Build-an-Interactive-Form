a833d0ab581bb1e62710f6019b47f80137cb6508
aakashsr <aakashsri.dev@gmail.com>
2018-07-12 20:12:53 +0530

    Feature added - Showing total amount dynamically as user click on items

--- js/app.js
+++ js/app.js
@@ -8,52 +8,55 @@ const design = document.getElementById('design');
 const color = document.getElementById('color');
 const fieldActivities = document.querySelector('.activities');
 const fieldsetInput = fieldActivities.querySelectorAll('input');
-console.log(fieldsetInput);
+const label = fieldActivities.getElementsByTagName('label');
 
-userName.setAttribute('autofocus' , 'autofocus');
 
-function createExtraDiv(){
-const input = document.createElement('input');
-input.type = 'text';
-input.placeholder = "Your job role";
-input.id = "other-title";
-input.name = 'title';
-fieldSet.appendChild(input);
-input.style.display = 'none';
-return input;
+let total = 0;
+
+userName.setAttribute('autofocus', 'autofocus');
+
+function createExtraDiv() {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = "Your job role";
+    input.id = "other-title";
+    input.name = 'title';
+    fieldSet.appendChild(input);
+    input.style.display = 'none';
+    return input;
 }
 
 createExtraDiv();
 
-selectTitle.addEventListener('change' , (e) => {
+selectTitle.addEventListener('change', (e) => {
 
-    if(e.target.value === 'other'){
+    if (e.target.value === 'other') {
         const input = document.querySelector('[placeholder = "Your job role"]');
         input.style.display = 'block';
     }
 });
 
-selectDesign.addEventListener('change' , (e) => {
-   
-    if(e.target.value === 'js puns'){
-       
-        for( i = 3 ; i < selectColor.options.length ; i++){ 
+selectDesign.addEventListener('change', (e) => {
+
+    if (e.target.value === 'js puns') {
+
+        for (i = 3; i < selectColor.options.length; i++) {
             selectColor.options[i].style.display = 'none';
         }
-        for( i = 0 ; i < 3 ; i++ ){
+        for (i = 0; i < 3; i++) {
             selectColor.options[i].style.display = 'block';
         }
         selectColor.options[0].selected = 'selected';
     }
 
-    if(e.target.value === 'heart js'){
-     
-        for( i = 0 ; i < 3 ; i++ ){
-            
+    if (e.target.value === 'heart js') {
+
+        for (i = 0; i < 3; i++) {
+
             selectColor.options[i].style.display = 'none';
         }
 
-        for( i = 3 ; i < selectColor.options.length ; i++){ 
+        for (i = 3; i < selectColor.options.length; i++) {
             selectColor.options[i].style.display = 'block';
         }
         selectColor.options[3].selected = 'selected';
@@ -61,13 +64,28 @@ selectDesign.addEventListener('change' , (e) => {
 });
 
 
-fieldActivities.addEventListener('change' , () => {
 
-    for( let i = 0 ; i < fieldsetInput.length ; i++){
-        if( fieldsetInput[i].checked){
-            console.log('yes');
+
+
+for (let i = 0; i < label.length; i++) {
+    fieldsetInput[i].addEventListener('click', () => {
+
+        const text = label[i].textContent;
+        if (fieldsetInput[i].checked) {
+            total += parseInt(text.substring(text.indexOf('$') + 1, text.length));
+            console.log(total);
         }
-    }
-    
+        const lastLabel = document.getElementById('totalamount');
+        lastLabel.textContent = total.toString();
+        
+    });
+}
 
-});
+function addTotalElement(){
+    const label = document.createElement('label');
+    label.id = "totalamount";
+    fieldActivities.appendChild(label);
+}
+addTotalElement();
+
+
